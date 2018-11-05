beb514dec38f551c52a85b21afda76999f2e1f69
Ewa Lorecka <lorecka.ewa@gmail.com>
2018-11-05 16:32:47 +0000

    delete second dog

--- dogs.js
+++ dogs.js
@@ -1,7 +1,6 @@
 console.log("This is my dogs.js file");
 console.log("Second line");
 console.log("first dog");
-console.log("second dog");
 console.log("third dog");
 console.log("fourth dog");
 console.log("fifth dog");
