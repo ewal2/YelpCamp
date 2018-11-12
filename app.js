1acc69d16c304b9e647f40950c70b9db4110a590
Ewa Lorecka <lorecka.ewa@gmail.com>
2018-11-12 00:17:22 +0000

    add { useNewUrlParser: true } to mongoose.connect

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
     
-mongoose.connect("mongodb://localhost:27017/yelp_camp_v9");
+mongoose.connect("mongodb://localhost:27017/yelp_camp_v9", { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
