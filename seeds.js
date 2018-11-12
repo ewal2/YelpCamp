59a68087ff650f1884a859e4751ddae587c7317e
Ewa Lorecka <lorecka.ewa@gmail.com>
2018-11-12 00:55:47 +0000

    update seedDB()

--- seeds.js
+++ seeds.js
@@ -18,11 +18,14 @@ var seeds = [
         image: "https://farm1.staticflickr.com/189/493046463_841a18169e.jpg",
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
     }
-]
+];
 
 function seedDB(){
     await Comment.remove({});
     await Campground.remove({});
+    for(const seed of seeds){
+       Campground.create(seed);
+    }
         
         data.forEach(function(seed){
             Campground.create(seed, function(err, campground){
