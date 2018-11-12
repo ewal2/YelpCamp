f4e6980a2e099fa11f4d6d74c603011b6da0fe64
Ewa Lorecka <lorecka.ewa@gmail.com>
2018-11-12 01:21:05 +0000

    add console.logs

--- seeds.js
+++ seeds.js
@@ -28,13 +28,16 @@ function seedDB(){
     
     for(const seed of seeds){
         let campground = await Campground.create(seed);
+        console.log("Campground created");
         let comment = await Comment.create(
             {
                 text: "This place is great, but I wish there was internet",
                 author: "Homer"
             })
+        console.log("Comment created");
         campground.comments.push(comment);
         campground.save();
+        console.log("Comment added to campground");
     }
 }
 
