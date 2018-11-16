f70221a76542cc90d8c30d79d7ef73104bb6255c
Ewa Lorecka <lorecka.ewa@gmail.com>
2018-11-16 12:49:13 +0000

    add return to res.redirect in user profile

--- routes/index.js
+++ routes/index.js
@@ -224,12 +224,12 @@ router.get("/users/:id", function(req, res) {
   User.findById(req.params.id, function(err, foundUser) {
     if(err) {
       req.flash("error", "Something went wrong.");
-      res.redirect("/");
+      return res.redirect("/");
     }
     Campground.find().where('author.id').equals(foundUser._id).exec(function(err, campgrounds) {
       if(err) {
         req.flash("error", "Something went wrong.");
-        res.redirect("/");
+        return res.redirect("/");
       }
       res.render("users/show", {user: foundUser, campgrounds: campgrounds});
     });
