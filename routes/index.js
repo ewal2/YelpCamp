e08b23037195ee3180d91101db50fa828abddc23
Ewa Lorecka <lorecka.ewa@gmail.com>
2018-11-16 12:36:16 +0000

    update routes/index

--- routes/index.js
+++ routes/index.js
@@ -71,34 +71,34 @@ router.get("/logout", function(req, res){
 });
 
 // forgot password
-router.get('/forgot', function(req, res) {
-  res.render('forgot');
-});
+//router.get('/forgot', function(req, res) {
+//  res.render('forgot');
+//});
 
-router.post('/forgot', function(req, res, next) {
-  async.waterfall([
-    function(done) {
-      crypto.randomBytes(20, function(err, buf) {
-        var token = buf.toString('hex');
-        done(err, token);
-      });
-    },
-    function(token, done) {
-      User.findOne({ email: req.body.email }, function(err, user) {
-        if (!user) {
-          req.flash('error', 'No account with that email address exists.');
-          return res.redirect('/forgot');
-        }
+//router.post('/forgot', function(req, res, next) {
+//  async.waterfall([
+//    function(done) {
+//      crypto.randomBytes(20, function(err, buf) {
+//        var token = buf.toString('hex');
+//        done(err, token);
+//      });
+//    },
+//    function(token, done) {
+//      User.findOne({ email: req.body.email }, function(err, user) {
+//        if (!user) {
+//          req.flash('error', 'No account with that email address exists.');
+//          return res.redirect('/forgot');
+//        }
 
-        user.resetPasswordToken = token;
-        user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+//        user.resetPasswordToken = token;
+//        user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
 
-        user.save(function(err) {
-          done(err, token, user);
-        });
-      });
-    },
-    function(token, user, done) {
+//        user.save(function(err) {
+//          done(err, token, user);
+//        });
+//      });
+//    },
+//    function(token, user, done) {
 //      var smtpTransport = nodemailer.createTransport({
 //        service: 'Gmail', 
 //        auth: {
@@ -106,64 +106,64 @@ router.post('/forgot', function(req, res, next) {
 //          pass: process.env.GMAILPW
 //        }
 //      });
-      var mailOptions = {
-        to: user.email,
-        from: 'lorecka.ewa@gmail.com',
-        subject: 'Node.js Password Reset',
-        text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
-          'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-          'http://' + req.headers.host + '/reset/' + token + '\n\n' +
-          'If you did not request this, please ignore this email and your password will remain unchanged.\n'
-      };
+//      var mailOptions = {
+//        to: user.email,
+//        from: 'lorecka.ewa@gmail.com',
+//        subject: 'Node.js Password Reset',
+//        text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+//          'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+//          'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+//          'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+//      };
       // wyskakuje mi bald mailgun is not a function
-      mailgun.messages.send(mailOptions, function(err) {
-        console.log('mail sent');
-        req.flash('success', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
-        done(err, 'done');
-      });
-    }
-  ], function(err) {
-    if (err) return next(err);
-    res.redirect('/forgot');
-  });
-});
+//      mailgun.messages.send(mailOptions, function(err) {
+//        console.log('mail sent');
+//        req.flash('success', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
+//        done(err, 'done');
+//      });
+//    }
+//  ], function(err) {
+//    if (err) return next(err);
+//    res.redirect('/forgot');
+//  });
+//});
 
-router.get('/reset/:token', function(req, res) {
-  User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
-    if (!user) {
-      req.flash('error', 'Password reset token is invalid or has expired.');
-      return res.redirect('/forgot');
-    }
-    res.render('reset', {token: req.params.token});
-  });
-});
+//router.get('/reset/:token', function(req, res) {
+//  User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
+//    if (!user) {
+//      req.flash('error', 'Password reset token is invalid or has expired.');
+//      return res.redirect('/forgot');
+//    }
+//    res.render('reset', {token: req.params.token});
+//  });
+//});
 
-router.post('/reset/:token', function(req, res) {
-  async.waterfall([
-    function(done) {
-      User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
-        if (!user) {
-          req.flash('error', 'Password reset token is invalid or has expired.');
-          return res.redirect('back');
-        }
-        if(req.body.password === req.body.confirm) {
-          user.setPassword(req.body.password, function(err) {
-            user.resetPasswordToken = undefined;
-            user.resetPasswordExpires = undefined;
+//router.post('/reset/:token', function(req, res) {
+//  async.waterfall([
+//    function(done) {
+//      User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
+//        if (!user) {
+//          req.flash('error', 'Password reset token is invalid or has expired.');
+//          return res.redirect('back');
+//        }
+//        if(req.body.password === req.body.confirm) {
+//          user.setPassword(req.body.password, function(err) {
+//            user.resetPasswordToken = undefined;
+//            user.resetPasswordExpires = undefined;
 
-            user.save(function(err) {
-              req.logIn(user, function(err) {
-                done(err, user);
-              });
-            });
-          });
-        } else {
-            req.flash("error", "Passwords do not match.");
-            return res.redirect('back');
-        }
-      });
-    },
-    function(user, done) {
+//            user.save(function(err) {
+//              req.logIn(user, function(err) {
+//                done(err, user);
+//              });
+//            });
+//          });
+//        } else {
+//            req.flash("error", "Passwords do not match.");
+//            return res.redirect('back');
+//        }
+  //    });
+  //  },
+    //function(user, done) {
 //      var smtpTransport = nodemailer.createTransport({
 //        service: 'Gmail', 
 //        auth: {
@@ -171,22 +171,22 @@ router.post('/reset/:token', function(req, res) {
 //          pass: process.env.GMAILPW
 //        }
 //      });
-      var mailOptions = {
-        to: user.email,
-        from: 'lorecka.ewa@gmail.com',
-        subject: 'Your password has been changed',
-        text: 'Hello,\n\n' +
-          'This is a confirmation that the password for your account ' + user.email + ' has just been changed.\n'
-      };
-      mailgun.messages.send(mailOptions, function(err) {
-        req.flash('success', 'Success! Your password has been changed.');
-        done(err);
-      });
-    }
-  ], function(err) {
-    res.redirect('/campgrounds');
-  });
-});
+      //var mailOptions = {
+        //to: user.email,
+        //from: 'lorecka.ewa@gmail.com',
+        //subject: 'Your password has been changed',
+        //text: 'Hello,\n\n' +
+        //  'This is a confirmation that the password for your account ' + user.email + ' has just been changed.\n'
+      //};
+      //mailgun.messages.send(mailOptions, function(err) {
+      //  req.flash('success', 'Success! Your password has been changed.');
+      //  done(err);
+      //});
+    //}
+  //], function(err) {
+    //res.redirect('/campgrounds');
+  //});
+//});
 
 //var data = {
 //  from: 'Ewa <lorecka.ewa@gmail.com>',
@@ -202,19 +202,19 @@ router.post('/reset/:token', function(req, res) {
 //  console.log(body);
 //});
 
-var transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-        type: 'OAuth2',
-        user: 'user@example.com',
-        clientId: '000000000000-xxx0.apps.googleusercontent.com',
-        clientSecret: 'XxxxxXXxX0xxxxxxxx0XXxX0',
-        refreshToken: '1/XXxXxsss-xxxXXXXXxXxx0XXXxxXXx0x00xxx',
-        accessToken: 'ya29.Xx_XX0xxxxx-xX0X0XxXXxXxXXXxX0x'
-    }
-});
+//var transporter = nodemailer.createTransport({
+//    host: 'smtp.gmail.com',
+//    port: 465,
+//    secure: true,
+//    auth: {
+//        type: 'OAuth2',
+//        user: 'user@example.com',
+//        clientId: '000000000000-xxx0.apps.googleusercontent.com',
+//        clientSecret: 'XxxxxXXxX0xxxxxxxx0XXxX0',
+//        refreshToken: '1/XXxXxsss-xxxXXXXXxXxx0XXXxxXXx0x00xxx',
+//        accessToken: 'ya29.Xx_XX0xxxxx-xX0X0XxXXxXxXXXxX0x'
+//    }
+//});
 
 
 
